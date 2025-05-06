894fc7b9f3c73692f5dfe2017613faf68a1ee929
rmishra <raghvendra.k.mishra@outlook.com>
2025-05-06 10:34:25 +0530

    Add firstName and lastName in User model

--- src/main/java/com/chatterbox/userservice/model/User.java
+++ src/main/java/com/chatterbox/userservice/model/User.java
@@ -1,8 +1,10 @@
 package com.chatterbox.userservice.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import lombok.Data;
 
 @Document(collection = "users_collection")
 @Data
@@ -10,8 +12,17 @@ public class User {
 	
 	@Id
     private String id;
-    
+
+    @NotBlank(message = "Username is mandatory")
     private String userName;
-    
+
+    @NotBlank(message = "First name is mandatory")
+    private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
     private String email;
 }
